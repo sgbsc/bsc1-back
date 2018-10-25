d05d1ca68d1214268108ddbc18c8f14cd3c8a82b
sgbsc <44087056+sgbsc@users.noreply.github.com>
2018-10-25 09:55:58 +0100

    Initial Commit of Application
    Initial creation of Spring Boot Maven project, with h2, JDBC and Web.

---
+++ src/main/java/com/sgbsc/Bsc1Application.java
@@ -0,0 +1,12 @@
+package com.sgbsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bsc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bsc1Application.class, args);
+	}
+}
---
+++ src/test/java/com/sgbsc/Bsc1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sgbsc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bsc1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
