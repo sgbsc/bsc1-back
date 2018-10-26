40195cd8e9e015491a6e97b33d11234984a41f04
sgbsc <44087056+sgbsc@users.noreply.github.com>
2018-10-26 11:18:56 +0100

    Added example sql files
    Added place-holder sql files to verify database work will be commited to the correct branch.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO stock values(1, 'VMWare', 'VMW', 56.05);
+INSERT INTO stock values(1, 'AT&T', 'T', 38.74);
+INSERT INTO stock values(1, 'Facebook', 'FB', 118.25);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE stock(
+	stock_id int,
+	company_name varchar2(50),
+	symbol varchar2(10),
+	price decimal(10,2)
+);
